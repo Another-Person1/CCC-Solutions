2c2c192cbf543fd4f3c0da422ce15e4ec1beb748
Another-Person1 <woodyl_2009@hotmail.com>
2024-05-21 09:27:07 -0400

    Create J2-Fergusonball Ratings.py

---
+++ 2022/Junior/J2-Fergusonball Ratings.py
@@ -0,0 +1,18 @@
+n = int(input(""))
+starrating = []
+for i in range(0, n):
+    points = int(input(""))
+    fouls = int(input(""))
+
+    stars = points*5-fouls*3
+    starrating.append(stars)
+
+starplayers = 0
+for playerstars in starrating:
+    if playerstars > 40:
+        starplayers += 1
+plus = ""
+if starplayers == n:
+    plus = "+"
+output = str(starplayers) + plus
+print(output)
